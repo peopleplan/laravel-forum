18efb2e22b4339dd5a13c88281e16769ea2095f6
Lloyd Nanka <lloyd.nanka@gmail.com>
2016-06-21 13:29:01 +1000

    add tenancy to the forums
    include the landlord package so we can have forums seperated by tenant

---
+++ migrations/2016_06_21_000937_add_tenancy_to_forum_table_threads.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTenancyToForumTableThreads extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('forum_threads', function (Blueprint $table)
+        {
+            $table->integer('tenant_id')->unsigned();
+
+            $table->foreign('tenant_id')
+                ->references('id')
+                ->on('tenants');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('forum_threads', function (Blueprint $table)
+        {
+            $table->dropForeign(['tenant_id']);
+            $table->dropColumn(['tenant_id']);
+        });
+    }
+}
